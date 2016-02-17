c5949663aed2ed610473f74d10d2bff4dd09d5af
Michael <michael.christian.frick@gmail.com>
2016-02-17 23:54:53 +0000

    api call

--- app/src/main/java/com/zooop/zooop_android/ui/fragments/DiggyFragment.java
+++ app/src/main/java/com/zooop/zooop_android/ui/fragments/DiggyFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.zooop.zooop_android.R;
-import com.google.android.gms.plus.PlusOneButton;
 
 public class DiggyFragment extends Fragment {
 
--- app/src/main/java/com/zooop/zooop_android/ui/fragments/DiscoverFragment.java
+++ app/src/main/java/com/zooop/zooop_android/ui/fragments/DiscoverFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.zooop.zooop_android.API;
 import com.zooop.zooop_android.R;
 
 public class DiscoverFragment extends Fragment {
@@ -16,6 +18,10 @@ public class DiscoverFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        API api = new API();
+        api.testAPI();
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_discover, container, false);
     }
